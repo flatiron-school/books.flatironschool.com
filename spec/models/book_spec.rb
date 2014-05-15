8b03875bc0bdfe08e3531eabaf4f950d21e115cc
Katie Hoffman <ktahoffman@gmail.com>
2014-05-15 12:33:11 -0400

    adds non-functional second test to calling pdf_url on book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -4,10 +4,25 @@ describe Book do
   context "with PDFs" do
     describe '#pdf_url' do
       it 'returns a url to the PDF type of the book' do
-        book = Book.create(:name => "Test Book")
-        books.types.create(:format => "PDF", "file" => "book.pdf")
+        book = Book.create(:title => "Test Book")
+        book.types.create(:format => "PDF", "file" => "book.pdf")
 
         expect(book.pdf_url).to eq("")
+      end
+    end
+    describe '#pdf_url' do
+      it 'returns a url to the PDF type of the book' do
+        book = Book.create(:title => "Test Book")
+        book.types.create(:format => "PDF", "file" => "book.pdf")
+        expect(book.pdf_url).to eq(
+          "https://" + 
+          ENV['AMAZON_BUCKET'] + 
+          ".s3.amazonaws.com/uploads/" +
+          book.types.first.class.name.underscore +
+          "/file/" +
+          "#{book.id}/" +
+          book.types.first.filename.to_s
+        )
       end
     end
   end
