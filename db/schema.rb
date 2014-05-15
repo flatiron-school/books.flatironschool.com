135c5a751e20bb0eb1bd488fec57470ee7b50519
Katie Hoffman <ktahoffman@gmail.com>
2014-05-15 15:31:50 -0400

    runs rake:db migrate after adding two friendly_id migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515144426) do
+ActiveRecord::Schema.define(version: 20140515190130) do
 
   create_table "book_tags", force: true do |t|
     t.integer  "book_id"
@@ -32,7 +32,23 @@ ActiveRecord::Schema.define(version: 20140515144426) do
     t.datetime "updated_at"
     t.string   "cover"
     t.boolean  "incomplete"
+    t.string   "slug"
   end
+
+  add_index "books", ["slug"], name: "index_books_on_slug"
+
+  create_table "friendly_id_slugs", force: true do |t|
+    t.string   "slug",                      null: false
+    t.integer  "sluggable_id",              null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope"
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "organizations", force: true do |t|
     t.datetime "created_at"
